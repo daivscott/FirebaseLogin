9952911149fa7233b3cf1c80957481ed9fc28aa2
daivscott <daivgit@mail.com>
2018-03-06 19:36:26 +0000

    First commit of 1page version
    Converted previous version to make a one page app

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB9XxjJY71tIGgvfPHu6DtM8CwCXVPLJHM",
+    authDomain: "loginproject-d76ae.firebaseapp.com",
+    databaseURL: "https://loginproject-d76ae.firebaseio.com",
+    projectId: "loginproject-d76ae",
+    storageBucket: "loginproject-d76ae.appspot.com",
+    messagingSenderId: "253817108391"
+};
+firebase.initializeApp(config);
+
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: 'loggedIn.html',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
